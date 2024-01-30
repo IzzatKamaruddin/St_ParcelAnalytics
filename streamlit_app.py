088281f3078b7ec32ba8192c6343e54da73e7dea
IzzatKamaruddin <73515475+IzzatKamaruddin@users.noreply.github.com>
2024-01-30 11:18:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,3 +102,22 @@ st_echarts.echart(filtered_df)
 st_echarts(
     options=filtered_df, height="400px",
 )
+
+
+
+option = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: 'bar'
+    }
+  ]
+};
+
